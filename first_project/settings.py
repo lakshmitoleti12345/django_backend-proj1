ee5d0dce30490fc19fa4ace4deea345eaa4b1fec
lakshmitoleti12345 <lakshmitoleti12345@gmail.com>
2026-09-05 16:58:28 +0530

    acess to origins

--- first_project/settings.py
+++ first_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-***94$m3lutrded*j-67==sfkmy2@3ijlrpnnd5wbo=nkq4p7v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
